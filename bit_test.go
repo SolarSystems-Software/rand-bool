9525f546f63caf3ac56fa9e74959d042ea9e700f
Levi <levi@crimsonaio.co>
2023-02-03 00:03:39 +1300

    Cleanup tests

--- bit_test.go
+++ bit_test.go
@@ -1,8 +1,29 @@
 package randbool
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
-func BenchmarkBits(b *testing.B) {
+func TestNextBit(t *testing.T) {
+	Default.NextBit()
+}
+
+func TestNextBitConcurrent(t *testing.T) {
+	var group sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		group.Add(1)
+		go func() {
+			t.Log("Bit:", Default.NextBit())
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+}
+
+func BenchmarkNextBit(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Default.NextBit()
 	}
--- default_impl_test.go
+++
@@ -1,9 +0,0 @@
-package randbool
-
-import "testing"
-
-func TestDefaultImplementation(t *testing.T) {
-	for i := 0; i < 5; i++ {
-		t.Log("Bit:", Default.NextBit())
-	}
-}
