8cc8afb405b0f6dbb1975749046aee2bf88b5ec7
Levi <levi@crimsonaio.co>
2023-02-02 23:53:25 +1300

    Update docs

--- rand_bool.go
+++ rand_bool.go
@@ -5,26 +5,27 @@ import (
 	"sync"
 )
 
-// BoolGenerator can be used to generate random bools and bits.
+// BoolGenerator is a generator capable of generating booleans and bits.
+//
+// All generators are safe for concurrent usage.
 type BoolGenerator struct {
-	sync.Mutex
-
+	mu            sync.Mutex
 	src           rand.Source
 	cache         int64
 	remainingBits int
 }
 
-// Default is a generally-available BoolGenerator.
-// Use New to create a new BoolGenerator.
+// Default is the default BoolGenerator.
+//
+// Use New to create your own generator.
 var Default = New()
 
 // NextBit gets the next random bit.
 func (generator *BoolGenerator) NextBit() uint8 {
-	// concurrency safety
-	generator.Lock()
-	defer generator.Unlock()
+	generator.mu.Lock()
+	defer generator.mu.Unlock()
 
-	// If there's no bits left, reset
+	// Reset if there's no bits left.
 	if generator.remainingBits == 0 {
 		generator.cache = generator.src.Int63()
 		generator.remainingBits = 63
@@ -36,7 +37,7 @@ func (generator *BoolGenerator) NextBit() uint8 {
 	// Shift over to the right by 1 for the next time this function is called
 	generator.cache >>= 1
 
-	// Decrement because we just used one
+	// Decrement remaining bits
 	generator.remainingBits--
 
 	return result
